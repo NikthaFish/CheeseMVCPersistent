082a634bd9b08726e6cc60288761dbaeedadeb52
Nikolas Fischer <fischern93@gmail.com>
2017-12-12 12:07:45 -0600

    Done Part 1 and most of Part 2

--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -10,6 +10,13 @@ namespace CheeseMVC.ViewModels
 {
     public class AddCheeseViewModel
     {
+        private IEnumerable<CheeseCategory> categories;
+
+        public AddCheeseViewModel(IEnumerable<CheeseCategory> categories)
+        {
+            
+        }
+
         [Required]
         [Display(Name = "Cheese Name")]
         public string Name { get; set; }
@@ -17,32 +24,10 @@ namespace CheeseMVC.ViewModels
         [Required(ErrorMessage = "You must give your cheese a description")]
         public string Description { get; set; }
 
-        public CheeseType Type { get; set; }
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
 
-        public List<SelectListItem> CheeseTypes { get; set; }
-
-        public AddCheeseViewModel() {
-
-            CheeseTypes = new List<SelectListItem>();
-
-            // <option value="0">Hard</option>
-            CheeseTypes.Add(new SelectListItem {
-                Value = ((int) CheeseType.Hard).ToString(),
-                Text = CheeseType.Hard.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Soft).ToString(),
-                Text = CheeseType.Soft.ToString()
-            });
-
-            CheeseTypes.Add(new SelectListItem
-            {
-                Value = ((int)CheeseType.Fake).ToString(),
-                Text = CheeseType.Fake.ToString()
-            });
-
-        }
+        public List<SelectListItem> Categories { get; set; }
     }
 }
